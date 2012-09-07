4f105a14d1d88095578d2fe64c55ed49321aa2b2
Matt Raibert <mattraibert@gmail.com>
2012-09-07 12:45:25 -0400

    build up a static bubble chart from scratch

--- force/force.js
+++ force/force.js
@@ -1,45 +1,25 @@
 var width = 1200,
     height = 850;
 
-var color = d3.scale.category20();
-
-var force = d3.layout.force()
-    .charge(-5000)
-    .linkDistance(50)
-    .gravity(1)
-    .size([width, height]);
-
 var svg = d3.select("#chart").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-d3.json("ps_final.json", function (json) {
-    force
-        .nodes(json.nodes)
-        .links(json.links)
-        .start();
-
-    var link = svg.selectAll("line.link")
-        .data(json.links)
-        .enter().append("line")
-        .attr("class", "link")
-        .style("stroke-width", 1);
-
-    var g = svg.selectAll("circle.node")
-        .data(json.nodes)
-        .enter().append("g")
-        .attr("class", "node")
-        .attr("transform", function (d) {
-            return "translate(" + (d.x * 400 + 100) + "," + (d.y * 400 + 100) + ")";
-        })
-        .call(force.drag);
-    var node = g.append("circle")
-        .attr("class", "node")
+function circle_style(circle) {
+    circle
         .attr("r", 25)
-        .style("fill", "lightyellow")
+        .style("fill", "lightgoldenrodyellow")
         .style("stroke", "darkgreen")
         .style("stroke-width", 1.5);
-
+}
+d3.json("ps_final.json", function (json) {
+    var g = svg.selectAll("g")
+        .data(json.nodes)
+        .enter().append("g")
+        .attr("transform", function (d) {
+            return "translate(" + (d.x * 500 + 100) + "," + (d.y * 500 + 100) + ")";
+        });
+    circle_style(g.append("circle"));
     g.append("text")
         .attr("text-anchor", "middle")
         .attr("dy", ".3em")
@@ -47,23 +27,15 @@ d3.json("ps_final.json", function (json) {
         .text(function (d) {
             return d.name;
         });
-
-    force.on("tick", function () {
-        link.attr("x1", function (d) {
-            return d.source.x;
+    var circle = svg.selectAll("circle")
+        .on("mouseover", function (data, index) {
+            circle = d3.select(this);
+            circle.style("fill", "steelblue");
+            circle.attr("r", 30);
         })
-            .attr("y1", function (d) {
-                return d.source.y;
-            })
-            .attr("x2", function (d) {
-                return d.target.x;
-            })
-            .attr("y2", function (d) {
-                return d.target.y;
-            });
-
-        g.attr("transform", function (d) {
-            return "translate(" + d.x + "," + d.y + ")";
-        });
-    });
+        .on("mouseout", function (data, index) {
+            circle = d3.select(this);
+            circle.style("fill", "lightgoldenrodyellow");
+            circle.attr("r", 25);
+        })
 });
