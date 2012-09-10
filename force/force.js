b2b52136318ec257a0a8c561d61b1a6b78975f82
Matt Raibert <mraibert@cyrusinnovation.com>
2012-09-10 15:53:05 -0400

    correct edges are highlighted

--- force/force.js
+++ force/force.js
@@ -19,24 +19,38 @@ function unhighlight(circle) {
         .style("stroke", "darkgreen")
         .style("stroke-width", 1.5);
 }
+
 function highlight(circle) {
     circle.style("fill", "steelblue");
+}
+
+function highlight_edge(edge) {
+    edge.style("stroke-width", 10);
+    edge.style("stroke", "darkgreen");
 }
 
 function has_endpoint_on(edge_data, node_data) {
     return edge_data.source === node_data || edge_data.target === node_data;
 }
+
 function find_incident_edges(node_data) {
     return svg.selectAll("line").filter(function (edge_data, index) {
-        return has_endpoint_on(edge_data, node_data);
+        var is_incident = has_endpoint_on(edge_data, node_data);
+        if(is_incident) {
+            var edge = d3.select("#edge_" + index);
+            highlight_edge(edge);
+        }
+        return is_incident;
     });
 }
+
 function style_adjacent_nodes(data, style_function) {
     find_incident_edges(data).each(function (edge_data) {
         style_function(d3.select("#node_" + edge_data.target.index).select("circle"));
         style_function(d3.select("#node_" + edge_data.source.index).select("circle"));
     });
 }
+
 d3.json("ps_final.json", function (json) {
     force
         .nodes(json.nodes)
