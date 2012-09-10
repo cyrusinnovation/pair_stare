8c5e3ebe0339fcc7d4030192c8095d77fa04a80c
Matt Raibert <mraibert@cyrusinnovation.com>
2012-09-10 16:12:39 -0400

    edges now unhighlight! (needs big time refactoring)

--- force/force.js
+++ force/force.js
@@ -20,6 +20,11 @@ function unhighlight(circle) {
         .style("stroke-width", 1.5);
 }
 
+function unhighlight_edge(edge) {
+    edge.style("stroke-width", 1);
+    edge.style("stroke", "darkgray");
+}
+
 function highlight(circle) {
     circle.style("fill", "steelblue");
 }
@@ -33,25 +38,35 @@ function has_endpoint_on(edge_data, node_data) {
     return edge_data.source === node_data || edge_data.target === node_data;
 }
 
+
 function find_incident_edges(node_data) {
-    return svg.selectAll("line").filter(function (edge_data, index) {
-        var is_incident = has_endpoint_on(edge_data, node_data);
-        if(is_incident) {
-            var edge = d3.select("#edge_" + index);
-            highlight_edge(edge);
-        }
-        return is_incident;
+    return svg.selectAll("line").filter(function (edge_data) {
+        return has_endpoint_on(edge_data, node_data);
     });
 }
 
-function style_adjacent_nodes(data, style_function) {
-    find_incident_edges(data).each(function (edge_data) {
+function style_adjacent_nodes(node_data, style_function) {
+    find_incident_edges(node_data).each(function (edge_data) {
         style_function(d3.select("#node_" + edge_data.target.index).select("circle"));
         style_function(d3.select("#node_" + edge_data.source.index).select("circle"));
     });
 }
 
+function style_incident_edges(node_data, style_function) {
+    return svg.selectAll("line").filter(function (edge_data, index) {
+        var is_incident = has_endpoint_on(edge_data, node_data);
+        if(is_incident) {
+            var edge = d3.select("#edge_" + index);
+            style_function(edge);
+        }
+        return is_incident;
+    });
+}
+
 d3.json("ps_final.json", function (json) {
+    linksos = json.links
+
+
     force
         .nodes(json.nodes)
         .links(json.links)
@@ -93,9 +108,11 @@ d3.json("ps_final.json", function (json) {
     svg.selectAll("g")
         .on("mouseover", function (node_data) {
             style_adjacent_nodes(node_data, highlight);
+            style_incident_edges(node_data, highlight_edge)
         })
         .on("mouseout", function (node_data) {
             style_adjacent_nodes(node_data, unhighlight);
+            style_incident_edges(node_data, unhighlight_edge)
         });
 
     force.on("tick", function () {
